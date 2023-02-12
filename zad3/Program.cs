87d9f63a9cd5f822b10139d38965abef85477c50
NEKETA <molnei1996@gmail.com>
2023-02-12 20:58:00 +0300

    added zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<1 || number>7)
+{
+    Console.WriteLine("такого дня нет");
+} else 
+{
+    switch(number)
+    {
+        case 1:
+            Console.WriteLine("нет");
+            break;
+        case 2:
+            Console.WriteLine("нет");
+            break;
+        case 3:
+            Console.WriteLine("нет");
+            break;
+        case 4:
+            Console.WriteLine("нет");
+            break;
+        case 5:
+            Console.WriteLine("нет");
+            break;
+        case 6:
+            Console.WriteLine("да");
+            break;
+        case 7:
+            Console.WriteLine("да");
+            break;
+    }
+} 
