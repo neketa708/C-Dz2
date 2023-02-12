468f2e779af16da942366df92039a3845155153b
NEKETA <molnei1996@gmail.com>
2023-02-12 20:37:08 +0300

    added zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int z = 10;
+int i = 0;
+if (number>=100)
+{
+     while (number>100)
+        {
+                i = number % z;
+                number /= 10;
+                
+        }
+        Console.WriteLine(i);
+} else
+{
+    Console.WriteLine("третьей цифры нет");
+}
