5aea82a03f7cb6b083a0f84884d9f0cd611297db
NEKETA <molnei1996@gmail.com>
2023-02-12 19:04:03 +0300

    added zad1

---
+++ zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<100)
+{
+    Console.WriteLine("число не трехзначное");
+} else if (number>999)
+{
+    Console.WriteLine("число не трехзначное");
+} else
+{
+int secondNumber = number/10%10;
+Console.WriteLine(secondNumber);
+}
